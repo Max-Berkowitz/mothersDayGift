12f16e55d116f565c1926d38303de9f75b6667ea
Max Berkowitz <maxl.berkowitz@gmail.com>
2018-05-11 14:38:49 -0700

    BugFix: added line breaks

--- mothersDayGift/thankYouMom.js
+++ mothersDayGift/thankYouMom.js
@@ -1,5 +1,5 @@
-document.writeln('Dear Uta,');
-document.writeln('');
-document.writeln('Thank you for being my mom, here is a little snippet of what I\'ve managed to learn thusfar');
-document.writeln('');
-document.writeln('Max');
+document.writeln('Dear Uta,<br>');
+document.writeln('<br>');
+document.writeln('Thank you for being my mom. Here is a little snippet of what I\'ve managed to learn thusfar!<br>');
+document.writeln('<br>');
+document.writeln('Max<br>');
