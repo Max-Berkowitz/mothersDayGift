4646043963359086c3ff9642faf2fde44ac06542
Max Berkowitz <maxl.berkowitz@gmail.com>
2018-05-11 14:32:12 -0700

    Added in the javascript

---
+++ mothersDayGift/thankYouMom.js
@@ -0,0 +1,5 @@
+document.writeln('Dear Uta,');
+document.writeln('');
+document.writeln('Thank you for being my mom, here is a little snippet of what I\'ve managed to learn thusfar');
+document.writeln('');
+document.writeln('Max');
