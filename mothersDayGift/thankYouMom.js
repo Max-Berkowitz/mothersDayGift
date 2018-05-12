8b575fff5a721acc14463e131decef412cf9c01f
Uta Grieshammer <ugrieshammer@Utas-MacBook-Air.local>
2018-05-11 22:52:57 -0700

    BugFix: fixed spelling error

--- mothersDayGift/thankYouMom.js
+++ mothersDayGift/thankYouMom.js
@@ -1,5 +1,5 @@
 document.writeln('Dear Uta,<br>');
 document.writeln('<br>');
-document.writeln('Thank you for being my mom. Here is a little snippet of what I\'ve managed to learn thusfar!<br>');
+document.writeln('Thank you for being my mom. Here is a little snippet of what I\'ve managed to learn thus far!<br>');
 document.writeln('<br>');
 document.writeln('Max<br>');
